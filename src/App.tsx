d32aab4dcb786e8bdacac76894eca6e4988d2e7d
morio-8 <morio.kunichide8@gmail.com>
2025-07-11 16:53:47 +0900

    reactへ移行

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+//表示テキストリスト
+const txtlist = [
+  "「ここにセリフが表示されます。」", 
+  "「2ページ目です」", 
+  "「3ページ目です」", 
+  "「4ページ目です」", 
+  "「5ページ目です」", 
+  "「6ページ目です」"
+];
+//画像リスト
+const imglist = [
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhwQ0Rt_D5XNRWZ9ml1fe39qosoTwotUwGotjtghs17btdS-iHGkU_2-05n56v3XRZoNfQ-FO7zNUNRDxdTkFvJhqvhlHwoaUyjrCyzEiOFPJ568w3PFs31k_z89fQ0eNppyrb93-26KTf1/s400/otaku_girl_fashion.png", 
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhrf9En3MJtd5ZzEa1Ky0jrp9rxFSTlwU30lzOToxwyVnSd7mprvlkTfhHuDDXFJfcAfF6o7oLbdQ_HL1OkA7n7M_rAVJTHOwg6pc-LO_QwoVEnqT1W3Lhk-KpbE62dvm_yvmu4qgDnass/s400/character_girl_normal.png", 
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgKm1v1NZfQDYxjXhOJZxdGMzff1-pRVIojAUTzdapA4FySgjo0mcLaLhJ4fL1Q5BQmgp2_KDhbkx11TaNY3ijlMPkTbXnoWqfO6EzJzL6xnsuv8xYLXCe1NjUGccOIBO53FxX_HXXOkCM/s400/character_boy_normal.png", 
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicsNz4LlhjifPxVYeISh950p2y6_b1au73MPdpCyCq4GCNWl7br_fWbK1U1PlzkZDegkSwQjchr7lQ7Sj8MsGxlzG006V-3hF4p_yFvqWgeBWudWRT1b3okL7yFgksU-5OCSz5G40HmA1C/s400/book_yomu_mushimegane.png", 
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhU91omJhkrJHCMw0SnS_1heTTQFDxcc1CjU9R5t9G2ikvHaNLO3UcH3BjarlsB2uQgFNwINxKKGuTDGoJvgmUEBD-qK5msqJFhYWKDSwM3HAadmE_KqtAxa58Mm73zQFqSbSqNa-j92ck/s400/shinpai_ojiisan.png", 
+  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiljmxhlIubmxVqsfpcm_vD70vLWHarnZuAeZ3J8znAcYAN6arMIMiUkpALqXoXM4nXEy1v1Ja7WF0PhYXB3SYUC69PXZW2ZbTQL8cqxleAZTHx4ExflWfhwRdl0tPwbHbtk2Ds68UjQCM/s400/hige_chobihige.png"
+];
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState("title");
+  const [pageNum, setPageNum] = useState(0);
+
+  //cookieを保存する
+  const saveButton = () => {
+    document.cookie = "page=" + pageNum;
+  };
+
+  //cookieを取得する
+  const continueButton = () => {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+      const [key, value] = cookie.split("=");
+      if (key === "page") {
+        setPageNum(parseInt(value));
+      }
+    }
+  };
+
+  const startButton = () => {
+    setPageNum(0);
+    setPage("game");
+  }
+
+  const nextButton = () => {
+    if(pageNum < txtlist.length-1){
+      setPageNum(pageNum + 1);
+    }
+  }
+
+  const backButton = () => {
+    if(pageNum > 0){
+      setPageNum(pageNum - 1);
+    }
+  }
+
+  if(page === "title"){
+    return (
+      <>
+        <div>
+          <ul><li>ゲームタイトル</li></ul>
+          <ul>
+            <a href="https://www.irasutoya.com/">素材：いらすとや</a>
+          </ul>
+        </div>
+        <div>
+          <button onClick={startButton}>はじめから</button>
+          <button onClick={continueButton}>つづきから</button>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={imglist[pageNum]} />
+        {/* <!-- キャラクター名 --> */}
+        <div>いらすとちゃん</div>
+        {/* <!-- セリフ --> */}
+        <div>{txtlist[pageNum]}</div>
+        <button onClick={nextButton}>次へ</button>
+        <button onClick={backButton}>戻る</button>
+        <button onClick={saveButton}>セーブ</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
