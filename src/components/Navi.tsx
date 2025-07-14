0f345698c672f1ef3e7957ec472000b6cf3c8120
morio-8 <morio.kunichide8@gmail.com>
2025-07-14 15:05:38 +0900

    components実装

---
+++ src/components/Navi.tsx
@@ -0,0 +1,30 @@
+import type { FC } from "react";
+
+type Props = {
+    page: string;
+    startButton?: () => void;
+    continueButton?: () => void;
+    nextButton?: () => void;
+    backButton?: () => void;
+    saveButton?: () => void;
+  };
+
+export const Navi: FC<Props> = (props) => {
+    const {page, startButton, continueButton, nextButton, backButton, saveButton} = props;
+  //ゲーム画面表示
+  if(page === "title"){
+    return (
+        <>
+            <button onClick={startButton}>はじめから</button>
+            <button onClick={continueButton}>つづきから</button>
+        </>
+      );
+  }
+  return (
+    <>
+        <button onClick={nextButton}>次へ</button>
+        <button onClick={backButton}>戻る</button>
+        <button onClick={saveButton}>セーブ</button>
+    </>
+  );
+};
